11b5a20e37cf89ebe437e2be8a7abbf50b60c3ec
ShrianshGoel <76607815+ShrianshGoel@users.noreply.github.com>
2021-12-05 17:03:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,345 @@
+var button, b1, b2, i2;
+var cross, iframe,iiw;
+var d = 1;
+var v1;
+var h=1
+var play, p1;
+var Heading
+var b1,b;
+var video;
+var cre;
+var j, j1;
+var a2;
+var purple;
+var s2;
+var v2
+var h3;
+var an = 2;
+var p2, p2, p3, p4;
+var animal, a1, h1, stat1us, j;
+function preload() {}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  button = createButton("");
+  a1 = createButton("");
+  a1.size(windowWidth / 1, windowHeight * 1.6);
+  a1.position(width / 4562.7, height / 54642.5);
+  a1.class("p");
+  a1.hide();
+  h1 = createElement("h1", "Axolotl the Walking Salamander");
+
+  heading=createElement("h1","Welcome To the Animal Infopedia. In construction but will finish soon. Till then enjoy available topics.")
+    heading.class("k3h");
+  heading.position(width/99999,height/9999999)
+  
+  purple = createButton("Purple Stripped Jellyfish");
+
+  purple.position(width / 2.5, height / 4);
+  purple.size(width / 4.2, height / 6.2);
+  purple.class("o2r");
+  h1.position(width / 3.7, height / 19);
+  h3 = createElement(
+    "h2",
+    "Axolotl is a type of salamander found in Lake Xochimilco. It is highly resistant to Cancers and can regenerate its limbs and other body parts. Staying in the water from almost its whole life has made it unable to survive on land for long times although after still having lungs like any other land and water animal. The axolotls are found in colors-black,golder,white and pink. The researchers are also researching on how to develop cure for cancers by seeing how does axolotl cure cancer. A sexually mature adult axolotl, at age 18–26 months, ranges in length from 15 to 45 cm (6 to 18 in), although a size close to 23 cm (9 in) is most common and greater than 30 cm (12 in) is rare. Axolotls possess features typical of salamander larvae, including external gills and a caudal fin extending from behind the head to the vent. Axolotls have barely visible vestigial teeth, which develop during metamorphosis. The primary method of feeding is by suction, during which their rakers interlock to close the gill slits. External gills are used for respiration, although buccal pumping (gulping air from the surface) may also be used to provide oxygen to their lungs.Today, the axolotl is still used in research as a model organism, and large numbers are bred in captivity. They are especially easy to breed compared to other salamanders in their family, which are rarely captive-bred due to the demands of terrestrial life. One attractive feature for research is the large and easily manipulated embryo, which allows viewing of the full development of a vertebrate. Axolotls are used in heart defect studies due to the presence of a mutant gene that causes heart failure in embryos. Since the embryos survive almost to hatching with no heart function, the defect is very observable.Also the Axolotl dosen't have any vocal sounds and may have a hiccup when going from water to land"
+  );
+  h3.hide();
+  h3.class("j");
+  h3.position(width / 3.7, height / 3.2);
+  button.size(width / 1, height / 1);
+  h1.class("h");
+  h1.hide();
+  button.position(width / 4562.7, height / 54642.5);
+  button.class("o");
+  animal = createButton("Axolotl");
+  animal.size(width / 4.2, height / 6.2);
+  animal.position(width / 12, height / 4);
+  animal.class("o2");
+  h2 = createButton("The Axolotl See in 3d");
+  h2.position(width / 122, height / 4.5);
+  h2.size(width / 4.2, height / 6.2);
+  h2.class("o2");
+  h2.hide();
+  stat1us = createButton("Axolotl:Critically Endangered");
+
+  stat1us.position(width / 129.5, height / 2);
+  stat1us.class("c1");
+  stat1us.size(width / 4.1, width / 12.2);
+  stat1us.hide();
+  video = createVideo(
+    "https://shrianshgoel.github.io/file-opener/Axolotls_%20The%20salamanders%20that%20snack%20on%20each%20other%20(but%20don't%20die)%20-%20Luis%20Zambrano.mp4"
+  );
+  video.position(width / 99999, height / 1.28);
+  video.size(width / 3.7, height / 3);
+  video.hide();
+  j = createButton("");
+  j.class("o3k");
+  j.position(width / 10, height / 1.1);
+  j.hide();
+  j1 = createButton("");
+  j1.class("o4k");
+  j1.position(width / 99, height / 1.02);
+  j1.hide();
+
+
+  a2 = createButton("");
+  a2.size(windowWidth / 1, windowHeight * 1.6);
+  a2.position(width / 33434343.7, height / 54642.5);
+  a2.class("p1");
+  a2.hide();
+  p2 = createElement("h1", "Purple Stripped Jellyfish");
+  p2.position(width / 3.7, height / 19);
+  p2.class("h");
+  p2.hide();
+  s2 = createButton("Purple Stripped Jellyfish: Least Concern");
+  s2.position(width / 129.5, height / 2);
+  s2.class("c1");
+  s2.size(width / 4.1, width / 12.2);
+  s2.hide();
+  p3=createButton("See 3d Model of Purple Stripped Jelly");
+ p3.position(width / 122, height / 4.5);
+  p3.size(width / 4.2, height / 6.2);
+  p3.class("o2r")
+  p3.hide()
+  p4=createElement("h3","The purple stripped jellyfish also known as purple stripped jelly is a type of Bioluminescent jellyfish found in the North Atlantic region, which includes the Mediterranean and Gulf of Mexico.Young cancer crabs are often found clinging to this jelly, even inside the gut. The crab helps the jelly by eating the parasitic amphipods that feed on and damage the jelly. Purple Stripped Jellys are fairly small jellyfish with adults having a bell diameter of 3–12 cm (1.2–4.7 in).Their colour is variable, ranging from mauve, purple, pink, light brown to yellow. The body is radially symmetrical. There is only one body cavity known as the gastrovascular cavity. This is a primitive gut or digestive cavity with only one opening that is used for ingestion and excretion; there are four long oral arms with crenulated margins that are the primary feeding surface. Each Purple Stripped Jellyfish medusa has eight long tentacles that emerge from the umbrella margin. Being radially symmetrical it has no head and thus no centralized nervous system. The nervous system present is primitive, consisting of a simple net composed of naked and largely non-polar neurons. In addition P. noctiluca also lacks a gaseous exchange, excretory and circulatory system. However cnidaria have evolved cnidae, cells which serve for a variety of functions that include prey capture, defense, locomotion and attachment. When fully formed cnidae are called cnidocytes. When stimulated the cnidae secrete nematocyst toxins that are biological poisons.Purple Stripped Jellyfish are bioluminescent, i.e. have an ability to produce a low light that is visible to humans during the night. Light is emitted in the form of flashes when the medusa is stimulated by turbulence created by waves or by a ship's motion. This flashing is only of relatively short duration and gradually fades. Purple Stripped Jellyfish is adapted to a pelagic, open-sea mode of life. Whereas most jellyfish, including the other species in the family Pelagiidae, have a life cycle with both the free-swimming stages (planula, ephyra and medusa) and a bottom-living polyp stage, P. noctiluca has adapted in such a way that the polyp stage is absent.Purple Stripped Jellyfish are opportunistic and have been recorded feeding on a wide range of small organisms like planktonic crustaceans (cladocerans, copepods, ostracods and crustacean larvae), mollusk larvae, larvaceans, hydromedusae, siphonophorans, arrow worms, fish eggs and fish larvae. Purple Stripped Jellyfish is considered the most important stinging jellyfish in the Mediterranean Sea, both its tentacles and—unusual among jellyfish—the bell are covered in cnidocytes (stinging cells), and even recently dead, stranded individuals can sting. The sting causes pain that typically lasts 1–2 weeks, local redness, swelling and a rash, but it is generally not dangerous and there are no known fatalities")
+   p4.class("u");
+  p4.position(width / 3.7, height / 3.2);
+  p4.hide()
+    v2 = createVideo(
+    "https://shrianshgoel.github.io/file-opener/j.mp4"
+  );
+  v2.position(width / 99999, height / 1.28);
+  v2.size(width / 3.7, height / 3);
+  v2.hide();
+    cre = createA(
+    "https://www.youtube.com/channel/UCsooa4yRKGN_zEE8iknghZA?sub_confirmation=1",
+    "This video Belongs to Ted-Ed ."
+  ).attribute("target", "blank_");
+  cre.position(width / 99, height / 0.9);
+  cre.class("a");
+  cre.hide();
+    b = createButton("");
+  b.class("o3k");
+  b.position(width / 10, height / 1.1);
+  b.hide();
+  b1 = createButton("");
+  b1.class("o4k");
+  b1.position(width / 99, height / 1.02);
+  b1.hide();
+}
+
+function draw() {
+  background(0);
+  b.mousePressed(
+  ()=>{
+    v2.play()
+    b.hide()
+    b1.show()
+  }
+  )
+    b1.mousePressed(
+  ()=>{
+    v2.pause()
+    b.show()
+    b1.hide()
+  }
+  )
+  purple.mousePressed(() => {
+    a2.show();
+    s2.show()
+    p4.show()
+    p3.show()
+    cre.show()
+    heading.hide()
+    b.show()
+    v2.show()
+    animal.hide();
+    p2.show();
+    button.hide();
+  });
+  if(h===1){
+    p3.mousePressed(
+    ()=>{
+      cre.hide()
+       iiw = createElement("iframe").attribute(
+        "src",
+        "https://sketchfab.com/models/81be051e683646d3922b2f6e71eafa11/embed?autostart=1&internal=1&tracking=0&ui_ar=0&ui_infos=0&ui_snapshots=1&ui_stop=0&ui_theatre=1&ui_watermark=0"
+      );
+      iiw.size(width / 1, height / 1);
+      iiw.position(width / 999, height / 999);
+
+      cross = createButton("");
+      cross.size(width / 14.5, height / 16);
+      cross.position(width / 1.2, height / 10);
+      cross.class("c");
+      h=0
+      a2.hide()
+      p4.hide()
+      p3.hide()
+      v2.hide()
+      s2.hide()
+      v2.pause()
+      b1.hide()
+      b.hide()
+    }
+    )
+    
+  }
+  if(h===0){
+    cross.mousePressed(
+    ()=>{
+      cross.hide()
+      iiw.hide()
+      cre.show()
+      a2.show()
+      v2.show()
+      p4.show()
+      p3.show()
+      b.show()
+      s2.show()
+      h=2
+    }
+    )
+  }
+  if(h===2){
+    p3.mousePressed(
+    ()=>{
+         a2.hide()
+      p3.hide()
+      cre.hide()
+      b.hide()
+      b1.hide()
+      s2.hide()
+      p4.hide()
+      v2.hide()
+      h=0
+      iiw.show()
+      cross.show()
+    }
+    )
+  }
+  j.mousePressed(() => {
+    video.play();
+    j.hide();
+    j1.show();
+  });
+  j1.mousePressed(() => {
+    video.pause();
+    j1.hide();
+    j.show();
+  });
+  animal.mousePressed(() => {
+    animal.hide();
+    button.hide();
+    video.show();
+    a1.show();
+    purple.hide();
+    cre.show();
+    h1.show();
+    j.show();
+    h3.show();
+    h2.show();
+    heading.hide()
+    stat1us.show();
+    an = 1;
+  });
+  if (d === 1) {
+    h2.mousePressed(() => {
+      d = 0;
+      stat1us.hide();
+      video.hide();
+      video.pause();
+      j.hide();
+      cre.hide();
+      iframe = createElement("iframe").attribute(
+        "src",
+        "https://sketchfab.com/models/41f4581b04844e0d83a19de42564d9e0/embed"
+      );
+      iframe.size(width / 1, height / 1);
+      iframe.position(width / 999, height / 999);
+
+      cross = createButton("");
+      cross.size(width / 14.5, height / 16);
+      cross.position(width / 1.2, height / 10);
+      cross.class("c");
+
+      a1.hide();
+      j1.hide();
+      h3.class("j");
+      h2.hide();
+      h3.hide();
+    });
+  }
+  if (d === 0) {
+    cross.mousePressed(() => {
+      j.show();
+      stat1us.show();
+      h2.show();
+      cross.hide();
+      cre.show();
+      h3.show();
+      video.show();
+      a1.show();
+      iframe.hide();
+    });
+    if (d === 0) {
+      h2.mousePressed(() => {
+        iframe.show();
+        j.hide();
+        cre.hide();
+        j1.hide();
+        video.hide();
+        video.pause();
+        stat1us.hide();
+        a1.hide();
+        h3.hide();
+        cross.show();
+      });
+    }
+  }
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  button.size(width / 1, height / 1);
+  if (an === 1) {
+  }
+
+  h3.position(width / 3.7, height / 3.2);
+  if (d === 0) {
+    cross.size(width / 14.5, height / 16);
+    cross.position(width / 1.2, height / 10);
+    iframe.size(width / 1, height / 1);
+    iframe.position(width / 999, height / 999);
+  }
+  purple.position(width / 2.5, height / 4);
+  purple.size(width / 4.2, height / 6.2);
+
+  video.position(width / 99999, height / 1.28);
+  video.size(width / 3.7, height / 3);
+  button.position(width / 4562.7, height / 54642.5);
+  a1.size(windowWidth, height * 1.5);
+  stat1us.position(width / 129.5, height / 2);
+  stat1us.size(width / 4.1, width / 12.2);
+  a1.position(width / 4562.7, height / 54642.5);
+  animal.size(width / 4.2, height / 6.2);
+  animal.position(width / 12, height / 4);
+  h1.position(width / 3.7, height / 19);
+  h2.position(width / 122, height / 4.5);
+  h2.size(width / 4.2, height / 6.2);
+    s2.size(width / 4.1, width / 12.2);
+    s2.position(width / 129.5, height / 2);
+  j.position(width / 10, height / 1.1);
+  j1.position(width / 99, height / 1.02);
+  cre.position(width / 99, height / 0.9);
+  p2.position(width / 3.7, height / 19);
+  a2.size(windowWidth / 1, windowHeight * 1.6);
+  a2.position(width / 3093802933.7, height / 54642.5);
+   p3.position(width / 122, height / 4.5);
+  p3.size(width / 4.2, height / 6.2);
+  p4.position(width / 3.7, height / 3.2);
+    heading.position(width/9999,height/9999999)
+  v2.position(width / 99999, height / 1.28);
+  v2.size(width / 3.7, height / 3);
+  b1.position(width / 99, height / 1.02);
+    b.position(width / 10, height / 1.1);
+}
